6b4969cc7e71f4905ba202c9110f0ee60e8935c9
matkolo1 <142239358+matkolo1@users.noreply.github.com>
2024-03-19 19:29:29 +0100

    1.

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ io.on('connection', (socket) => {
   socket.on('createGame', (gameName, playerName, symbol) => {
     const gameId = generateGameId();
     socket.join(gameId);
-    games[gameId] = { player1: [playerName, symbol], player2: [], name: gameName };
+    games[gameId] = { player1: [playerName, symbol], player2: [], name: gameName , ready1: false, ready2: false};
     socket.emit('gameCreated', gameId);
     console.log(gameId, games[gameId]);
   });
@@ -32,23 +32,33 @@ io.on('connection', (socket) => {
     if (games[gameId] && !games[gameId].player2[0]) {
       games[gameId].player2.push(playerName);
       socket.join(gameId);
-      io.to(gameId).emit('playerJoined', playerName);
+      io.to(gameId).emit('playerJoined', playerName, games[gameId].name);
       console.log(games[gameId])
     } else {
       console.log(`Game ${gameId} full.`)
-      socket.emit('full', true)
+      socket.emit('full', true, games[gameId].name)
     }
   });
+
+  socket.on('ready', (gameId, mas, playerName) => {
+    console.log(`Player ${playerName} (${mas}) is ready.`)
+    if (mas) games[gameId].ready1 = true;
+    else games[gameId].ready2 = true;
+    io.to(gameId).emit('ready', mas, playerName);
+    if (games[gameId].ready1 && games[gameId].ready2) {
+      io.to(gameId).emit('start', JSON.stringify(games[gameId]))
+    }
+  })
 
   // Obsluha události odpojení klienta
   socket.on('disconnect', () => {
     console.log('Client disconnected:', socket.id);
   });
+
 });
 
 // Funkce pro generování náhodného ID hry
 function generateGameId() {
   return Math.random().toString(36).substr(2, 9);
 }
-
 
--- test/client.js
+++ test/client.js
@@ -5,7 +5,11 @@ var game = {
   gameId: null,
   gameName: null,
   playerName: null,
+  enemyName: null,
+  start: 0,
 }
+var master = true;
+var ready = false;
 
 const params = new URLSearchParams(window.location.search);
 // Získání hodnoty parametru "gameId" z URL
@@ -16,18 +20,21 @@ if (game.gameId) joinGame(true)
 socket.on('gameCreated', (gameId) => {
   console.log('Game created:', gameId);
   // Zobrazení odkazu na připojení k hře
-  const joinUrl = window.location.href + 'index.html?gameId=' + gameId;
+  var url = window.location.href.split('index')[0]
+  const joinUrl = url + 'index.html?gameId=' + gameId;
   console.log('Join URL:', joinUrl);
   game.gameId = gameId
-  console.log(game)
-  document.getElementById("demo").innerHTML = `Created room ${game.gameName}(${game.gameId}) under name ${game.playerName}.`;
+  document.getElementById("info").innerHTML = `Created room ${game.gameName}(${game.gameId}) under name ${game.playerName}.`;
 
   // Zde můžete provést další manipulace s odkazem (např. zobrazení na stránce)
 });
 
 // Obsluha události připojení nového hráče k hře
-socket.on('playerJoined', (playerName) => {
-  console.log('Player joined:', playerName);
+socket.on('playerJoined', (playerName, gameName) => {
+  console.log('Player joined:', playerName, gameName);
+  game.gameName = gameName;
+  document.getElementById("info").innerHTML = `Joined room ${game.gameName}(${game.gameId}) under name ${game.playerName}.`
+
   // Zde můžete provést akce související s novým hráčem (např. aktualizace seznamu hráčů)
 });
 
@@ -51,11 +58,33 @@ function joinGame(id) {
   const playerName = prompt('Enter your name:');
   socket.emit('joinGame', game.gameId, playerName);
   game.playerName = playerName
-  document.getElementById("demo").innerHTML = `Joined room ${game.gameName}(${game.gameId}) under name ${game.playerName}.`
-  socket.on('full', (istrue) => {
-    if (istrue) {
-      console.log(`Game ${game.gameId} is full.`)
-      document.getElementById("demo").innerHTML = `Room ${game.gameName}(${game.gameId}) is full.`
-    }
-  })
+  master = false;
 }
+
+function readd() {
+  socket.emit('ready', game.gameId, master, game.playerName)
+  ready = true;
+}
+
+socket.on('ready', (mas, playerName) => {
+  console.log(`Player ${playerName}(${mas}) is ready`)
+})
+
+socket.on('full', (istrue, gameName) => {
+  if (istrue) {
+    console.log(`Game ${gameName}(${game.gameId}) is full.`)
+    game.gameName = gameName
+    document.getElementById("info").innerHTML = `Room ${game.gameName}(${game.gameId}) is full.`
+  }
+})
+
+socket.on('start', (games) => {
+  games = JSON.parse(games)
+  if (master) {
+    game.enemyName = games.player2[0]
+    if (games.player1[1] = 'O') game.start = 1
+  } else {
+    game.enemyName = games.player1[0]
+    if (games.player1[1] = 'X') game.start = 1
+  }
+})
